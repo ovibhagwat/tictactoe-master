e37b1d1da95e309cc5c2934532441d6b5ac0f36d
ovibhagwat <ovib@outlook.com>
2022-05-27 09:54:11 -0700

    removed teacher

--- lib/viewmodels/game_viewmodel.dart
+++ lib/viewmodels/game_viewmodel.dart
@@ -1,48 +1,32 @@
 import 'package:skyline_template_app/core/utilities/route_names.dart';
 import 'package:skyline_template_app/viewmodels/base_viewmodel.dart';
 import 'package:skyline_template_app/core/enums/view_state.dart';
-import 'package:skyline_template_app/model/Teacher.dart';
 import 'package:skyline_template_app/core/services/navigation_service.dart';
 import 'package:skyline_template_app/locator.dart';
-import 'package:faker/faker.dart';
 
 class GameViewModel extends BaseViewModel {
-  final  _navigationService = locator<NavigationService>();
-  List<Teacher> teachers = [];
+  final _navigationService = locator<NavigationService>();
   bool pageLoaded = false;
 
   Future init() async {
     print("Game ViewModel init()");
     setState(ViewState.Busy);
     try {
-_generateTeacherList(5);
-pageLoaded = true;
+      pageLoaded = true;
     } catch (e) {
       setState(ViewState.Error);
     }
     setState(ViewState.Idle);
-
   }
 
-  List _generateTeacherList(int teacherCount) {
-    if (!pageLoaded == true)
-    for (int i =0; i < teacherCount; i++){
-      var faker = new Faker();//this package generates fictitious info...
-      Teacher teacher = new Teacher(uid: 'xxx$i', firstName: faker.person.firstName(), lastName: faker.person.lastName(), nickName: 'Cool Teacher $i', email: faker.internet.email() );
-    teachers.add(teacher);
-    }
-    teachers.forEach((element) {print('${element.firstName}, ${element.lastName}');});
-    return teachers;
-  }
-  void tapped(){
+  void tapped() {
     //what happens when player taps screen
     //CheckWinner();
     //if CheckWinner = false, computerPlays();
     notifyListeners();
   }
 
-  void routeToHomeView(){
+  void routeToHomeView() {
     _navigationService.navigateTo(HomeViewRoute);
   }
-
 }
